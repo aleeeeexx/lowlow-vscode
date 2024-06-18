ee8e92faa0d1c55e93105477d83fc5cb3db452e8
陈谦 <chenq178@vanke.com>
2024-06-18 11:14:30 +0800

    feat: cookie no check

--- src/webview-vue/src/utils/cookie.ts
+++ src/webview-vue/src/utils/cookie.ts
@@ -1,17 +1,18 @@
+//@ts-nocheck
 export function setCookie(name: string, value: string) {
-  document.cookie = name + "=" + value;
+  document.cookie = name + '=' + value
 }
 
 export function getCookie(name: string) {
-  const reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-  const arr = document.cookie.match(reg);
+  const reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+  const arr = document.cookie.match(reg)
   if (arr) {
-    return arr[2];
+    return arr[2]
   } else {
-    return null;
+    return null
   }
 }
 
 export function delCookie(name: string) {
-  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
 }
